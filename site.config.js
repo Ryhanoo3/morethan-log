1b3edf8923be3feffd5cfe1299080e5dee7bcdda
Ryhanoo3 <149338016+Ryhanoo3@users.noreply.github.com>
2024-12-28 00:08:20 +0000

    changed profile picture

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Ryhan A",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/profile-picture.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Cybersecurity Professional",
     bio: "Sharing my learnings in staying secured online",
     email: "ryhanahmed03@outlook.com",
